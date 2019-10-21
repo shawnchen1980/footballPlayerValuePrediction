92de984b76d16df1979498e2d5c1403b8df13731
Shawnchen <shawnchen1980@hotmail.com>
2019-10-21 15:30:36 +0800

    init

--- dataClean.py
+++ dataClean.py
@@ -81,8 +81,71 @@ cols=[
  'goalkeeping_positioning',
  'goalkeeping_reflexes']
 
+colsC=[
+ 'overall',
+ 'potential',
+ 'value_eur',
+ 'wage_eur',
+ 'international_reputation',
+ 'weak_foot',
+ 'skill_moves',
+ 'shooting',
+ 'passing',
+ 'dribbling',
+ 'defending',
+ 'physic',
+ 'gk_diving',
+ 'gk_handling',
+ 'gk_kicking',
+ 'gk_reflexes',
+ 'gk_speed',
+ 'gk_positioning',
+ 'attacking_crossing',
+ 'attacking_finishing',
+ 'attacking_heading_accuracy',
+ 'attacking_short_passing',
+ 'attacking_volleys',
+ 'skill_dribbling',
+ 'skill_curve',
+ 'skill_fk_accuracy',
+ 'skill_long_passing',
+ 'skill_ball_control',
+ 'movement_acceleration',
+ 'movement_sprint_speed',
+ 'movement_agility',
+ 'movement_reactions',
+ 'movement_balance',
+ 'power_shot_power',
+ 'power_jumping',
+ 'power_stamina',
+ 'power_strength',
+ 'power_long_shots',
+ 'mentality_aggression',
+ 'mentality_interceptions',
+ 'mentality_positioning',
+ 'mentality_vision',
+ 'mentality_penalties',
+ 'mentality_composure',
+ 'defending_marking',
+ 'defending_standing_tackle',
+ 'defending_sliding_tackle',
+ 'goalkeeping_diving',
+ 'goalkeeping_handling',
+ 'goalkeeping_kicking',
+ 'goalkeeping_positioning',
+ 'goalkeeping_reflexes']
+
+#如何看df的每一列是什么类型？
+#df.dtypes
+
+#如何向df中某个位置插入一列
+#df.insert(loc=新列的位置,column=列名,value=整数或者序列)
+
+#如何看df中某一列中是否包含某字符串
+#df.col.str.contains("xxx",regex=False)
+
 df1=pd.read_csv('datasets/players_19.csv',index_col=0)
-df1=df1.loc[:,['value_eur']]
+df1=df1.loc[:,colsC]
 df2=pd.read_csv('datasets/players_18.csv',index_col=0)
-df2=df2.loc[:,['value_eur']]
+df2=df2.loc[:,colsC]
 df3=df1-df2
