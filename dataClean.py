ac20e759503f80a6b1962e087937e1b4e01ed544
unknown <shawnchen1980@hotmail.com>
2019-12-13 00:27:23 +0800

    dataclean

--- dataClean.py
+++ dataClean.py
@@ -81,7 +81,17 @@ cols=[
  'goalkeeping_positioning',
  'goalkeeping_reflexes']
 
+colsA=[
+ 'short_name',
+ 'long_name',
+ 'nationality',
+ 'club']
+
 colsC=[
+ 'club',
+ 'age',
+ 'height_cm',
+ 'weight_kg',
  'overall',
  'potential',
  'value_eur',
@@ -94,12 +104,6 @@ colsC=[
  'dribbling',
  'defending',
  'physic',
- 'gk_diving',
- 'gk_handling',
- 'gk_kicking',
- 'gk_reflexes',
- 'gk_speed',
- 'gk_positioning',
  'attacking_crossing',
  'attacking_finishing',
  'attacking_heading_accuracy',
@@ -135,6 +139,49 @@ colsC=[
  'goalkeeping_positioning',
  'goalkeeping_reflexes']
 
+colsD=[
+ 'age',
+ 'height_cm',
+ 'weight_kg',
+ 'value_eur',
+ 'wage_eur',
+ 'international_reputation',
+ 'weak_foot',
+ 'skill_moves',
+ 'attacking_crossing',
+ 'attacking_finishing',
+ 'attacking_heading_accuracy',
+ 'attacking_short_passing',
+ 'attacking_volleys',
+ 'skill_dribbling',
+ 'skill_curve',
+ 'skill_fk_accuracy',
+ 'skill_long_passing',
+ 'skill_ball_control',
+ 'movement_acceleration',
+ 'movement_sprint_speed',
+ 'movement_agility',
+ 'movement_reactions',
+ 'movement_balance',
+ 'power_shot_power',
+ 'power_jumping',
+ 'power_stamina',
+ 'power_strength',
+ 'power_long_shots',
+ 'mentality_aggression',
+ 'mentality_interceptions',
+ 'mentality_positioning',
+ 'mentality_vision',
+ 'mentality_penalties',
+ 'mentality_composure',
+ 'defending_marking',
+ 'defending_standing_tackle',
+ 'defending_sliding_tackle',
+ 'goalkeeping_diving',
+ 'goalkeeping_handling',
+ 'goalkeeping_kicking',
+ 'goalkeeping_positioning',
+ 'goalkeeping_reflexes']
 #如何看df的每一列是什么类型？
 #df.dtypes
 
@@ -146,7 +193,13 @@ colsC=[
 
 #如何看df中不同列的数据类型
 #df.dtypes
-
+#kmeans分类
+#https://github.com/jcalcutt/projects/blob/master/kmeans_clustering/kmeans_fifa.ipynb
+#回归
+#https://blog.uruit.com/soccer-and-machine-learning-tutorial/
+#分析
+#https://jiayiwangjw.github.io/2017/06/12/SoccerDataAnalysis/
+#https://www.kaggle.com/roshansharma/fifa-data-visualization
 def toValue(str):
     if(str.find("+")>=0):
         arr=str.split("+")
@@ -159,15 +212,12 @@ def toValue(str):
     
 import numpy as np
 df1=pd.read_csv('datasets/players_19.csv',index_col=0)
-df1=df1.loc[:,colsC]
-for col in df1.columns:
+df1=df1.dropna(axis=1)
+df11=df1.loc[:,colsA]
+df12=df1.loc[:,colsC]
+for col in df12.columns[1:]:
     if(df1[col].dtype==np.object):
-        df1[col]=df1[col].apply(toValue)
+        df12[col]=df12[col].apply(lambda x : eval(str(x)))
+df12 = df12.groupby('club').transform(lambda x: (x - x.mean()) / x.std())
+df1=df11.join(df12)
         
-df2=pd.read_csv('datasets/players_18.csv',index_col=0)
-df2=df2.loc[:,colsC]
-for col in df2.columns:
-    if(df2[col].dtype==np.object):
-        df2[col]=df2[col].apply(toValue)
-df3=df1-df2
-df3=df3[np.isnan(df3['overall'])==False]
